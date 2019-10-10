312135f6837505ea83420d02e1ab75340ebc0905
Bartek <bartis312123@gmail.com>
2019-10-10 14:34:27 +0200

    poprawione zadanie 9

--- cw1/zad8_9.py
+++ cw1/zad8_9.py
@@ -16,8 +16,31 @@ slownik_studentow=[{
     "adres": "ul. Koralowa 58 11-023 Warszawa",
     "email": "bartekkoziolek@gmail.com",
     "rok_urodzenia": "10.10.2019"
-    }
-
+    },
+    145625:{
+    "imię": "Szymon",
+    "nazwisko": "Horbal",
+    "wiek": 23,
+    "adres": "ul. Koralowa 25 11-023 Warszawa",
+    "email": "szymonhorbal@gmail.com",
+    "rok_urodzenia": "10.10.2013"
+    },
+    145862:{
+    "imię": "Kamil",
+    "nazwisko": "Gwiazda",
+    "wiek": 24,
+    "adres": "ul. Koralowa 45 11-023 Warszawa",
+    "email": "kamilgwiazda@gmail.com",
+    "rok_urodzenia": "10.10.2018"
+    },
+    145236:{
+    "imię": "Jan",
+    "nazwisko": "Nowak",
+    "wiek": 25,
+    "adres": "ul. Koralowa 78 11-023 Warszawa",
+    "email": "jannowak@gmail.com",
+    "rok_urodzenia": "10.10.2017"
+    },
 
 
 
