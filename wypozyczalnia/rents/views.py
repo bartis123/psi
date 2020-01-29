951309d05cfec9aefc4c8fafa550a4e935a7d0b5
unknown <bartoszkoziolek1998@gmail.com>
2020-01-29 18:10:33 +0100

    poprawa w widokach

--- wypozyczalnia/rents/views.py
+++ wypozyczalnia/rents/views.py
@@ -5,8 +5,6 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import generics
-from rest_framework import status
 from rest_framework.decorators import api_view
 from django.http import Http404
 # Create your views here.
